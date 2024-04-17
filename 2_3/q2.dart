a6d272da7d587c88baf1996b65798a001637ff58
prachi <144036679+prachimanani01@users.noreply.github.com>
2024-04-17 12:05:05 +0530

    Add files via upload

---
+++ 2_3/q2.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+void main() {
+    List<String> myList = [];
+    stdout.write("Enter Number of elements you want to add in List : ");
+    int len = int.parse(stdin.readLineSync()!);
+    for(int i = 1 ;i <= len; i++){
+      stdout.write("Enter the String $i Value : ");
+      String str = stdin.readLineSync()!;
+      myList.add(str);
+      print("");
+    }
+    myList = myList.toSet().toList();
+    myList.forEach((element) {
+      print("$element");
+    });
+}
