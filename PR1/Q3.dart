0e09546e4bc93db06263bba374fdcf0a25045d7b
prachi <144036679+prachimanani01@users.noreply.github.com>
2024-04-26 19:32:28 +0530

    Update Q3.dart

--- PR1/Q3.dart
+++ PR1/Q3.dart
@@ -1,39 +1,42 @@
 import 'dart:io';
 
 void main() {
-  int Insert, Delete, Update, View, choice;
-
-  print("press 1 for Insert..");
-  print("press 2 for Delete..");
-  print("press 3 for Update..");
-  print("press 4 for View..");
-
-  stdout.write("Enter your choice :- ");
-  choice = int.parse(stdin.readLineSync()!);
-
-  List<int> demo = [1, 2, 3];
+  List array = [1, 2, 3, 4];
+  print("1. Enter an element ");
+  print("2. Update an element ");
+  print("3. Remove an element ");
+  print("4. View the elements ");
+  stdout.write("Enter Your Choice : ");
+  int choice = int.parse(stdin.readLineSync()!);
   switch (choice) {
     case 1:
-      demo.add(4);
-      print("$demo");
+      stdout.write("Enter the Element  : ");
+      int addVar = int.parse(stdin.readLineSync()!);
+      array.add(addVar);
+      print(array);
       break;
-
     case 2:
-      demo.remove(1);
-      print("$demo");
+      stdout.write("Enter the Index you want to Update : ");
+      int index = int.parse(stdin.readLineSync()!);
+      print("");
+      stdout.write("Enter the Element in that index : ");
+      int newEle = int.parse(stdin.readLineSync()!);
+      array[index] = newEle;
+      print(array);
       break;
-
     case 3:
-      demo.replaceRange(1, 3, [2, 3]);
-      print("$demo");
+      stdout.write("Enter the Element you want to remove: ");
+      int removeVar = int.parse(stdin.readLineSync()!);
+      array.remove(removeVar);
+      print(array);
       break;
-
     case 4:
-      demo.add(3);
-      print("$demo");
+      stdout.write("Here it is : ");
+      array.forEach((element) {
+        stdout.write("$element  ");
+      });
       break;
     default:
-      stdout.write("invlid choise...!!");
-      break;
+      print("Invalid Choice!!");
   }
 }
