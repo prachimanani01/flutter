dd3cd5ff7d3570e0757e9c155390c47be131dc65
prachi <144036679+prachimanani01@users.noreply.github.com>
2024-04-26 19:31:35 +0530

    Update Q1.dart

--- PR1/Q1.dart
+++ PR1/Q1.dart
@@ -1,12 +1,23 @@
 import 'dart:io';
 
-void main(){
-  List<int> array = [1,2,-3,4,6,-7,8,-9];
-  print("Original List : ${array}");
-  print("Negative Elements in Array : ");
-  for(int i in array){
-    if(i<0){
-      stdout.write("${i} ");
-    }
+void main() {
+  List array = [];
+  stdout.write("Enter the element number you want : ");
+  int length = int.parse(stdin.readLineSync() ?? "0");
+  for (int i = 1; i <= length; i++) {
+    stdout.write("Enter Element : ");
+    int elem = int.parse(stdin.readLineSync() ?? "0");
+    array.add(elem);
   }
+  print("Elements : ");
+  array.forEach((element) {
+    stdout.write("$element   ");
+  });
+  print("");
+  print("Negative elements is : ");
+  array.forEach((element) {
+    if (element < 0) {
+      stdout.write("$element");
+    }
+  });
 }
